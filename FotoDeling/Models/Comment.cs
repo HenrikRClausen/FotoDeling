337ee06256bfa9646327abfbbfe668f9c654b5b2
Henrik Ræder Clausen <henrik@fangorn.dk>
2018-12-17 11:26:19 +0100

    Nogle opdateringer

---
+++ FotoDeling/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FotoDeling.Models {
+    public class Comment {
+        public int CommentID { get; set; }
+        public int PhotoID { get; set; }
+        public string UserName { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        virtual public Photo Billede { get; set; }
+        }
+    }
